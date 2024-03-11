c7e7d9c2f5cc8d43f0a07e259094d12fca0e0fb1
Michael Kremenetsky <28833521+michaelkremenetsky@users.noreply.github.com>
2024-03-10 23:11:50 -0700

    Fix fill on Bookmark Icon

--- apps/web/features/bookmarks/BookmarkButton.tsx
+++ apps/web/features/bookmarks/BookmarkButton.tsx
@@ -58,12 +58,11 @@ export const BookmarkButton = ({
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                fill="none"
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
                 shapeRendering="geometricPrecision"
-                className="h-6 w-6 stroke-sky-500"
+                className="h-6 w-6 fill-sky-500 stroke-sky-500"
               >
                 <path
                   strokeLinecap="round"
