346f6c59ec3e51dd576f701ccc9aa70c37108339
Michael Kremenetsky <28833521+michaelkremenetsky@users.noreply.github.com>
2024-03-21 06:44:05 -0700

    Remove old sorting options

--- apps/web/stores/ui.ts
+++ apps/web/stores/ui.ts
@@ -11,10 +11,6 @@ export const hideSidebar = atomWithStorage<boolean>("hideSidebar", false);
 export const Sort = atom<
   | "Latest"
   | "Oldest"
-  | "Readability Ascending"
-  | "Readability Descending"
-  | "Content Length Ascending"
-  | "Content Length Descending"
 >("Latest");
 
 export const kmenu = atom<boolean | undefined>(false);
--- packages/atoms/feedsAtom.ts
+++ packages/atoms/feedsAtom.ts
@@ -1,12 +1,5 @@
 import { atom } from "jotai";
 
-export const sortAtom = atom<
-  | "Latest"
-  | "Oldest"
-  | "Readability Ascending"
-  | "Readability Descending"
-  | "Content Length Ascending"
-  | "Content Length Descending"
->("Latest");
+export const sortAtom = atom<"Latest" | "Oldest">("Latest");
 
 export const titleAtom = atom<string | undefined>(undefined);
